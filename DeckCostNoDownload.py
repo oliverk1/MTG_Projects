260062da4a702454375387f543d6f4c7c3fac3e6
Oliver King <116508612+oliverk1@users.noreply.github.com>
2023-08-21 13:35:07 +0100

    Add files via upload

--- DeckCostNoDownload.py
+++ DeckCostNoDownload.py
@@ -15,7 +15,7 @@ def collection():
     return collectionList
 
 def deck():
-    file = open("deck.txt", "r")
+    file = open("./deck.txt", "r")
     deck = file.read()
     deckList = deck.split("\n")
     deckList = [i.split(" ", 1) for i in deckList]
@@ -34,6 +34,7 @@ def cardInCollection(collectionList, deckList):
                 collectionList.remove(i)
                 deckList.remove(row)
             elif row[1] == i[1] and numDeck > numOwned:
+                print(numDeck,numOwned)
                 cardsMissing.append([(numDeck - numOwned), row[1]])
                 collectionList.remove(i)
                 deckList.remove(row)
@@ -44,28 +45,36 @@ def cardInCollection(collectionList, deckList):
             cardsMissing.append(row)
     return cardsMissing
 
+def getPrice(info,row):
+    if 'prices' in info:
+        prices = info['prices']
+        priceNormal = str(prices['eur'])
+        if priceNormal == "None":
+            priceNormal = "NA"
+        else:
+            priceNormal = round((float(prices['eur'])*0.86326101), 2)
+            priceNormal = float(priceNormal) * float(row[0])
+    else:
+        priceNormal = "NA"
+    return priceNormal
+
 def getCardInfo(cardsMissing):
     count = 0
     for row in cardsMissing:
         card = row[1]
         response = requests.get("https://api.scryfall.com/cards/named?fuzzy="+card)
         info = response.json()
-        prices = info['prices']
-        priceNormal = str(prices['eur'])
-        if priceNormal == "None":
-            priceNormal = "N/A"
-        else:
-            priceNormal = round((float(prices['eur'])*0.86326101), 2)
-            priceNormal = float(priceNormal) * float(row[0])
+        priceNormal = getPrice(info,row)
         cardsMissing[count].append(priceNormal)
         count = count + 1
     return cardsMissing
+
 
 def userInfo(cardsMissing):
     total = 0
     for row in cardsMissing:
         print(str(row[0])+"x",str(row[1])+",","Cost: £"+str(row[2]))
-        if str(row[2]) != "N/A":
+        if str(row[2]) != "NA":
             total = total + float(row[2])
     print("\nTotal cost of missing cards: £"+str(round(total, 2)))
 
